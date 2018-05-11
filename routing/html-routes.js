19930973916664301fd4323bc856ac502a4f3e6e
Sam <samueljacksonhicks@gmail.com>
2018-05-11 14:25:54 -0500

    propper links

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,9 +1,6 @@
 module.exports = function(app){
 	
 	app.get("/", (req, res) => {
-		res.send(`
-<h1>Index page</h1>
-<p>this is the index page with the route '/'. It is the default page.</p>
-						`);
+		res.render("index");
 	})
 }
