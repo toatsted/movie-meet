1a4fb9df433d97abf77120b217f024b5baa62e21
Sam <samueljacksonhicks@gmail.com>
2018-05-11 12:33:52 -0500

    adding more html to test stuff

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,8 +1,9 @@
 module.exports = function(app){
 	
 	app.get("/", (req, res) => {
-					res.send(`
-					<h1>Index page</h1>
-									`);
+		res.send(`
+<h1>Index page</h1>
+<p>this is the index page with the route '/'. It is the default page.</p>
+						`);
 	})
 }
